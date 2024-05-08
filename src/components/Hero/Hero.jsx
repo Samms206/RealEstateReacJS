c964e037ffa4fa6d1955f036c45031be940a09ee
Samms206 <sh2069936@gmail.com>
2024-05-08 21:15:56 +0700

    added-HeroSection

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,66 @@
 import React from "react";
 import "./Hero.css"
+import {HiLocationMarker} from "react-icons/hi"
+import CountUp from "react-countup";
 
 const Hero = () => {
     return (
         <section className="hero-wrapper">
             <div className="paddings innerWidth flexCenter hero-container">
-                <div className="flexColCenter hero-left">
-                    <h1 className="hero-heading">
-                        Find, Buy and Sell <br /> Your Property with us 
-                    </h1>
-                    <p className="hero-description">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
-                    <button className="button">
-                        <a href="#">Explore</a>
-                    </button>
+                {/* Left Side */}
+                <div className="flexColStart hero-left">
+                    <div className="hero-title">
+                        <div className="orange-circle"/>
+                        <h1>Find, Buy and Sell <br /> Your Property <br />with us </h1>
+                    </div>
+                    <div className="flexColStart hero-desc">
+                        <span className="secondaryText">Find a variety of properties that suit you and your needs</span>
+                        <span className="secondaryText">Forget All The difficult in finding a residence for you</span>
+                    </div>
+                    <div className="flexCenter search-bar">
+                        <HiLocationMarker color="var(--blue)" size={25}/>
+                        <input type="text" />
+                        <button className="button">Search</button>
+                    </div>
+                    <div className="flexCenter stats">
+                    <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={8000} end={9100} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">
+                                Premium Products
+                            </span>
+                        </div>
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp start={1900} end={2235} duration={4} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">
+                                Happy Customers
+                            </span>
+                        </div>
+                        <div className="flexColCenter stat">
+                            <span>
+                                <CountUp end={82} />
+                                <span>+</span>
+                            </span>
+                            <span className="secondaryText">
+                                Award Winnings
+                            </span>
+                        </div>
+                    </div>
                 </div>
+                {/* end of left side */}
+
+                 {/* Right Side */}
                 <div className="flexCenter hero-right">
                     <div className="image-container">
                         <img src="./hero-image.png" alt="hero" />
                     </div>
                 </div>
+                {/* end right side */}
             </div>
         </section>
     )
