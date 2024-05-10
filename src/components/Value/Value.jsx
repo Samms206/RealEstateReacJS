82cdcb5ec08ae759109e27f77abc05092e7188f0
Samms206 <sh2069936@gmail.com>
2024-05-11 00:32:57 +0700

    added-accordion

---
+++ src/components/Value/Value.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+    AccordionItemState
+} from 'react-accessible-accordion';
+import "react-accessible-accordion/dist/fancy-example.css";
+import {MdOutlineArrowDropDown} from 'react-icons/md'
+import "./Value.css"
+import { data } from '../../utils/accordion';
+
+const Value = () => {
+  return (
+    <sectoin className="v-wrapper">
+        <div className="paddings innerWidth flexCenter v-container">
+            {/* left side */}
+            <div className="v-left">
+                <div className="image-container">
+                    <img src="./value.png" alt="value" />
+                </div>
+            </div>
+            {/* right side */}
+            <div className="flexColStart v-right">
+                <span className="orangeText">Our Value</span>
+                <span className="primaryText">Value We Give To You</span>
+                <span className="secondaryText">
+                    We always ready to help. You just need to <br />
+                    we believe in doing business with you
+                </span>
+
+                <Accordion 
+                    className="accordion"
+                    allowMultipleExpanded={false}
+                    preExpanded={[0]}
+                    >
+                    {
+                        data.map((item, index) => {
+                            return(
+                                <AccordionItem className='accordionItem'>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            <div className="flexCenter icon">{item.icon}</div>
+                                            <div className="flexColStart details">
+                                                <span className="secondaryText">{item.heading}</span>
+                                                <span className="primaryText">{item.detail}</span>
+                                            </div>
+                                            <div className="flexCenter icon">
+                                                <MdOutlineArrowDropDown/>
+                                            </div>
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                </AccordionItem>
+                            )
+                            })
+                    }
+                </Accordion>
+            </div>
+        </div>
+    </sectoin>
+  )
+}
+
+export default Value
