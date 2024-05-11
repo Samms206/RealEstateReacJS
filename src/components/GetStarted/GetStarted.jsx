2052e50935d3021cd3553801564f3be7d43ff596
Samms206 <sh2069936@gmail.com>
2024-05-11 15:27:22 +0700

    updated-GetStartedSection

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './GetStarted.css'
 
 const GetStarted = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='g-wrapper'>
+        <div className='paddings innerWidth g-container'>
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started with Homyz</span>
+                <span className='secondaryText'>
+                    Subscribe and find super attractive places to stay and live.
+                    <br />
+                    Find your residence soon
+                </span>
+                <button className="button">
+                    <a href="#">Get Started</a>
+                </button>
+            </div>
+        </div>
+    </section>
   )
 }
 
