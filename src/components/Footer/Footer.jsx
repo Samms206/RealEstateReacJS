01d58f5cd347d818a379b290513e3f7a26dc64e1
Samms206 <sh2069936@gmail.com>
2024-05-11 15:38:07 +0700

    updated-Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* Left Side */}
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="logo" width={120} />
+          <span className="secondaryText">
+            Our vision is to make life easier for everyone
+            <br />
+            the best properties for everyone
+          </span>
+        </div>
 
-export default Footer
+        {/* Right Side */}
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">145 New York, FL 4571, USA</span>
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
